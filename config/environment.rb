9d746492a7722ae8dc00d65b7d59125b90c8717a
Iddahodhiambo <Iddahawuor@yahoo.com>
2023-03-07 18:35:04 +0300

    establish db connection

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,8 @@
 # This is an _environment variable_ that is used by some of the Rake tasks to determine
 # if our application is running locally in development, in a test environment, or in production
 ENV['RACK_ENV'] ||= "production"
+connection_details = YAML::load(File.open('config/database.yml'))
+ActiveRecord::Base.establish_connection(connection_details[RACK_ENV])
 
 # Require in Gems
 require 'bundler/setup'
