205839fa4bc2494ef07eb47d07736b8833444bc7
Iddahodhiambo <Iddahawuor@yahoo.com>
2023-03-07 18:43:23 +0300

    add yaml gem

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,6 @@
 # This is an _environment variable_ that is used by some of the Rake tasks to determine
 # if our application is running locally in development, in a test environment, or in production
+require 'yaml'
 ENV['RACK_ENV'] ||= "production"
 connection_details = YAML::load(File.open('config/database.yml'))
 ActiveRecord::Base.establish_connection(connection_details[RACK_ENV])
