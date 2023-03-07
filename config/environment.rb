cc230862881133c7c31b0929514fef304a8de3c3
Iddahodhiambo <Iddahawuor@yahoo.com>
2023-03-07 18:57:28 +0300

    require active_record

--- config/environment.rb
+++ config/environment.rb
@@ -1,6 +1,7 @@
 # This is an _environment variable_ that is used by some of the Rake tasks to determine
 # if our application is running locally in development, in a test environment, or in production
 require 'yaml'
+require 'active_record'
 ENV['RACK_ENV'] ||= "production"
 connection_details = YAML::load(File.open('config/database.yml'))
 ActiveRecord::Base.establish_connection(connection_details[RACK_ENV])
